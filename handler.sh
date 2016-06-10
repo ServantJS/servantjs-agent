8e59d5c4f6c9a3871feca6e4f44cdf250761b5be
Vitaly Orlov <vitaliuorlov@gmail.com>
2016-06-10 14:02:55 +0300

    add update option

--- handler.sh
+++ handler.sh
@@ -1,20 +1,50 @@
 #!/usr/bin/env bash
 
 START_SCRIPT="servantjs-agent.js"
-INSTALL_DIR="/usr/local/servantjs/agent"
 
-cd ${INSTALL_DIR}
+SERVANT_DIR="/usr/local/servantjs"
+INSTALL_DIR="${SERVANT_DIR}/agent"
+GIT_NAME="${SERVANT_DIR}/servantjs-agent"
+LINK_PATH="/usr/local/bin/servant-agent"
+BACKUP_DIR="${SERVANT_DIR}/.agent_backup"
+INSTALL_BACKUP_FILE="${SERVANT_DIR}/.agent_install.sh"
 
 function start {
+    cd ${INSTALL_DIR}
     forever start ${START_SCRIPT}
 }
 
 function stop {
+    cd ${INSTALL_DIR}
     forever stop ${START_SCRIPT}
 }
 
 function status {
+    cd ${INSTALL_DIR}
     forever list | grep "pid\|${START_SCRIPT}"
+}
+
+function update {
+    cp -r ${INSTALL_DIR} ${BACKUP_DIR}
+    rm -f ${LINK_PATH}
+
+    cp ${INSTALL_DIR}/install.sh ${INSTALL_BACKUP_FILE}
+
+    rm -fr ${INSTALL_DIR}
+
+    bash ${INSTALL_BACKUP_FILE}
+
+    if [ $? != 0 ]; then
+        rm -fr ${GIT_NAME}
+        rm -fr ${INSTALL_DIR}
+        ln -s ${INSTALL_DIR}/handler.sh ${LINK_PATH}
+        mv ${BACKUP_DIR} ${INSTALL_DIR}
+    else
+        cp ${BACKUP_DIR}/config.json ${INSTALL_DIR}/
+        rm -fr ${BACKUP_DIR}
+    fi
+
+    rm -f ${INSTALL_BACKUP_FILE}
 }
 
 case "$1" in
@@ -30,6 +60,9 @@ case "$1" in
     ;;
   status)
     status
+    ;;
+  update)
+    update
     ;;
   *)
     echo "Usage: servant-agent <start|stop|restart|status>"
